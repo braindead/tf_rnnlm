0deadc56b5d88f32e124cbe1b104e4579d4ebe7f
Rajiv Poddar <rajiv@scribie.com>
2017-03-20 13:07:27 +0530

    added flask wrapper

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import tensorflow as tf
+from deploy import load_graph
+from deploy import test
+from flask import request
+from flask import Response
+import pickle
+import json
+import os
+
+app = Flask(__name__)
+
+graph = load_graph("models/sc-small-model-t/frozen_model.pb")
+
+with open("models/sc-small-model-t/word_to_id", 'r') as f:
+    word_to_id = pickle.load(f)
+
+@app.route("/perplexity", methods=['POST'])
+def perplexity():
+    f = request.files['file']
+    f.save(f.filename)
+
+    with open(f.filename) as fn:
+        content = fn.readlines()
+
+    p = [test(graph, word_to_id, line) for line in content]
+
+    os.remove(f.filename)
+    
+    return Response(json.dumps(p), mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run()
