918959ec08e95be39d9f25c9a810ae2b81a312a4
Rajiv Poddar <rajiv@scribie.com>
2017-04-05 18:13:15 +0000

    use same phrases at each run

--- eval.sh
+++ eval.sh
@@ -6,21 +6,20 @@ then
     threshold=$1
 fi
 
-file='../../phrases-new.csv'
-if [ -f $2 ]
-then
-    file=$2
-fi
-
+file='../phrases5-shuf.csv'
 
 echo -n "bad phrases at $threshold: "
-for i in {1..5}
+for i in {0..4}
 do
-    shuf -n 100 $file | cut -d '	' -f1 | python word_lm.py --action ppl --ppl_threshold $threshold --ppl_compare gt --model_dir models/latest 2>/dev/null | wc -l
+    range_start=$((i*100+1))
+    range_end=$(((i+1)*100))
+    sed -n -e "${range_start},${range_end}p" $file | cut -d '	' -f1 | python word_lm.py --action ppl --ppl_threshold $threshold --ppl_compare gt --model_dir models/latest 2>/dev/null | wc -l
 done | awk '{ sum += $1; n++; printf "%d ", $1 } END { if (n > 0) printf "AVG: %.2f%\n", sum / n; }'
 
 echo -n "good phrases at $threshold: "
-for i in {1..5}
+for i in {0..4}
 do
-    shuf -n 100 $file | cut -d '	' -f2 | python word_lm.py --action ppl --ppl_threshold $threshold --ppl_compare lt --model_dir models/latest 2>/dev/null | wc -l
+    range_start=$((i*100+1))
+    range_end=$(((i+1)*100))
+    sed -n -e "${range_start},${range_end}p" $file | cut -d '	' -f2 | python word_lm.py --action ppl --ppl_threshold $threshold --ppl_compare lt --model_dir models/latest 2>/dev/null | wc -l
 done | awk '{ sum += $1; n++; printf "%d ", $1 } END { if (n > 0) printf "AVG: %.2f%\n", sum / n; }'
